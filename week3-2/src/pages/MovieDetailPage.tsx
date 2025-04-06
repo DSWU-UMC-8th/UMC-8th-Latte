35ff98e3343d45c38d16462a524889d063351dfe
soyyyyy <da5868@naver.com>
2025-04-06 18:37:40 +0900

    ✨ Feat: 로딩 에러 처리 및 여러개의 페이지 라우팅 처리
    Feat: 로딩 에러 처리 및 여러개의 페이지 라우팅 처리

---
+++ week3-2/src/pages/MovieDetailPage.tsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+
+export const MovieDetailPage = () => {
+    const params = useParams();
+
+    return (
+        <div>
+            상세 
+        </div>
+    );
+};
