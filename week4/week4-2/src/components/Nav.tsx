aa57e43877764a35c5972f15c03e2bf6d0f228e2
soyyyyy <da5868@naver.com>
2025-04-13 21:31:35 +0900

    ✨ Feat: Nav 페이지 링크 연결
    Feat: Nav 페이지 링크 연결

--- week4/week4-2/src/components/Nav.tsx
+++ week4/week4-2/src/components/Nav.tsx
@@ -1,4 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+    const navigate = useNavigate();
+
+    const handleNavigate = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const id = e.currentTarget.id;
+
+        if (id === 'login') navigate('/login');
+        else if (id === 'signup') navigate('/signup');
+    };
+
     return (
         <nav className="w-full bg-black/90 text-white px-8 py-4 flex items-center justify-between shadow-md">
             <div className="text-pink-500 font-bold text-xl">
@@ -6,12 +17,18 @@ const Navbar = () => {
             </div>
 
             <div className="flex gap-2">
-                <button className="bg-gray-800 hover:bg-gray-700 text-white px-4 py-2 rounded">
-                    로그인
+                <button 
+                    id="login" 
+                    onClick={handleNavigate}
+                    className="bg-gray-800 hover:bg-gray-700 text-white px-4 py-2 rounded">
+                        로그인
                 </button>
 
-                <button className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded">
-                    회원가입
+                <button 
+                    id="signup" 
+                    onClick={handleNavigate}
+                    className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded" >
+                        회원가입
                 </button>
             </div>
         </nav>
