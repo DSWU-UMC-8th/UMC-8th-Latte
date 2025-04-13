bfd38c8e60c67e090e8e591aa7d6473c8561c98f
soyyyyy <da5868@naver.com>
2025-04-13 21:34:21 +0900

    🐛 Feat: Nav 링크 수정 및 pointer
    Feat: Nav 링크 수정 및 pointer

--- week4/week4-2/src/components/Nav.tsx
+++ week4/week4-2/src/components/Nav.tsx
@@ -6,28 +6,32 @@ const Navbar = () => {
     const handleNavigate = (e: React.MouseEvent<HTMLButtonElement>) => {
         const id = e.currentTarget.id;
 
-        if (id === 'login') navigate('/login');
+        if (id === 'logo') navigate('/');
+        else if (id === 'login') navigate('/login');
         else if (id === 'signup') navigate('/signup');
     };
 
     return (
         <nav className="w-full bg-black/90 text-white px-8 py-4 flex items-center justify-between shadow-md">
-            <div className="text-pink-500 font-bold text-xl">
-                UMC-8th
-            </div>
+            <button 
+                id="logo" 
+                onClick={handleNavigate}
+                className="text-pink-500 font-bold text-xl cursor-pointer">
+                    UMC-8th
+            </button>
 
             <div className="flex gap-2">
                 <button 
                     id="login" 
                     onClick={handleNavigate}
-                    className="bg-gray-800 hover:bg-gray-700 text-white px-4 py-2 rounded">
+                    className="bg-gray-800 hover:bg-gray-700 text-white px-4 py-2 rounded cursor-pointer">
                         로그인
                 </button>
 
                 <button 
                     id="signup" 
                     onClick={handleNavigate}
-                    className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded" >
+                    className="bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded cursor-pointer" >
                         회원가입
                 </button>
             </div>
