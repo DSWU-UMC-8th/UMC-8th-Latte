5e7dfdaf15e1a689f37dfc95fc41ad408d6c8934
soyyyyy <da5868@naver.com>
2025-04-13 21:24:47 +0900

    💄 Fix: 홈페이지 ui 약간 수정
    Fix: 홈페이지 ui 약간 수정

--- week4/week4-2/src/pages/HomePage.tsx
+++ week4/week4-2/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 const HomePage = () => {
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center h-full gap-4
+            w-full bg-black text-white">
             홈 페이지
         </div>
     )
