bf0b1e8cc31e36c578bb3f6e482018e8cc103966
soyyyyy <da5868@naver.com>
2025-05-10 02:33:35 +0900

    🎨 Feat: 5주차 코드 세팅
    feat: 5주차 코드 세팅

---
+++ week6/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-black/90 text-gray-400 text-sm text-center py-4">
+            ⓒ 2025 UMC-8th Latte
+        </footer>
+        );
+    };
+
+export default Footer;
